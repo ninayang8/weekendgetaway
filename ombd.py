2c9ebc704b1b376c96a7e0d3b371fdd0e119927d
Nina Yang <ninayang@umich.edu>
2021-04-17 01:28:13 -0400

    get_title_and_rating, create_database

--- ombd.py
+++ ombd.py
@@ -1,8 +1,14 @@
 import json
 import os
 import requests
+import sqlite3
 
 API_KEY = "2a2f5075"
+
+def create_request_url(title):
+    base_url = 'http://www.omdbapi.com/?t={}&apikey=2a2f5075&type=movie&plot=short&r=json'
+    request_url = base_url.format(title)
+    return request_url
 
 def open_file():
     root_path = os.path.dirname(os.path.abspath(__file__))
@@ -17,11 +23,28 @@ def open_file():
         list.append(movie)
     return list
 
+def get_title_and_rating():
+    d = {}
+    movie_list = open_file()
+    for movie in movie_list:
+        url = create_request_url(movie)
+        try:
+            r = requests.get(url)
+            data = json.loads(r.text)
+            if data == {"Response":"False", "Error":"Movie not found!"}:
+                continue
+            else:
+                d[movie] = data['imdbRating']
+        except:
+            return None
+    print(d)
+    return d     
 
-def create_request_url(title):
-    base_url = 'http://www.omdbapi.com/?t={}&apikey=2a2f5075&type=movie&plot=short&r=json'
-    request_url = base_url.format(title)
-    return request_url
+def create_database():
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+'movies.db')
+    cur = conn.cursor()
+    return cur, conn
 
 
 # def request_data(url):
@@ -31,6 +54,7 @@ def create_request_url(title):
 #     return r.text
 def main():
     open_file()
+    get_title_and_rating()
 
 if __name__ == "__main__":
     main()
