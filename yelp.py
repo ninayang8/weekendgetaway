8076427572c944b4c0b6819858fdfdd8c8daef22
andrsn <andrsn@umich.edu>
2021-04-23 17:21:24 -0400

    figure 1 draft

--- yelp.py
+++ yelp.py
@@ -9,6 +9,8 @@ import os
 import json
 import requests
 import sqlite3
+import numpy as np
+import matplotlib.pyplot as plt
 
 API_KEY = "YuijH5s1qzWZVSOfuGknf5--ccUjwSLWR2XDFJSrghEuRipM8ouL-wm4-ib3ARRdqTZVW3Pd8rMoA4jPf6I6DKBTOCu4AdorRGMVPv1PL1SWssRnjXfSDip1ACh6YHYx"
 CLIENT_ID = "IGLDk-j90I9w05zBZUo5qw"
@@ -29,10 +31,8 @@ def setUpDatabase(db_name):
     conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
 
-    cur.execute('DROP TABLE IF EXISTS Restaurants')
     cur.execute('CREATE TABLE IF NOT EXISTS Restaurants ("restaurant_id" TEXT PRIMARY KEY, "name" TEXT, "category" TEXT, "rating" REAL, "price" TEXT)')
 
-    cur.execute('DROP TABLE IF EXISTS Locations')
     cur.execute('CREATE TABLE IF NOT EXISTS Locations ("restaurant_id" TEXT PRIMARY KEY, "name" TEXT, "location" TEXT, "address" TEXT, "latitude" REAL, "longitude" REAL)')
 
     conn.commit()
@@ -50,18 +50,42 @@ def addEntriesToDatabase(cur, conn, data, location):
 
     conn.commit()
 
+def RatingVsPricePlot(cur, conn):
+
+    rating = []
+    price = []
+
+    cursor = cur.execute("SELECT rating, price FROM Restaurants")
+    for row in cursor:
+        rating.append(row[0])
+        price.append(row[1])
+    
+    values = {}
+    for x in range(len(price)):
+        if len(price[x]) not in values:
+            values[len(price[x])] = []
+        values[len(price[x])].append(rating[x])
+
+    for x in values.keys():
+        values[x] = sum(values[x]) / len(values[x])
+    
+    plt.bar(values.keys(), values.values())
+    plt.show()
+
+
 def main():
 
     cur, conn = setUpDatabase("Database.db")
 
-    for x in range(4):
+    # for x in range(4):
 
-        url = get_url('Ann Arbor', x * 25)
-        data = request_data(url)
-        data = json.loads(data)
+    #     url = get_url('Ann Arbor', x * 25)
+    #     data = request_data(url)
+    #     data = json.loads(data)
 
-        addEntriesToDatabase(cur, conn, data, "Ann Arbor")
+    #     addEntriesToDatabase(cur, conn, data, "Ann Arbor")
 
+    RatingVsPricePlot(cur, conn)
 
 
 if __name__ == "__main__":
