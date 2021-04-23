9ea1e07007b8bff012d7672fe512fa420c42cf6c
andrsn <andrsn@umich.edu>
2021-04-23 15:13:41 -0400

    add database

--- yelp.py
+++ yelp.py
@@ -5,44 +5,47 @@
 # 04.16.2021   
 #
 
+import os
 import json
 import requests
+import sqlite3
 
 API_KEY = "YuijH5s1qzWZVSOfuGknf5--ccUjwSLWR2XDFJSrghEuRipM8ouL-wm4-ib3ARRdqTZVW3Pd8rMoA4jPf6I6DKBTOCu4AdorRGMVPv1PL1SWssRnjXfSDip1ACh6YHYx"
 CLIENT_ID = "IGLDk-j90I9w05zBZUo5qw"
 
-# Reading and Writing to a Cache
-def read_cache(CACHE_FNAME):
+def get_url(location):
 
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    CACHE_FNAME = dir_path + '/' + "cache_yelp.json"
-    try:
-        cache_file = open(CACHE_FNAME, 'r', encoding="utf-8") # Try to read the data from the file
-        cache_contents = cache_file.read()  # If it's there, get it into a string
-        CACHE_DICTION = json.loads(cache_contents) # And then load it into a dictionary
-        cache_file.close() # Close the file, we're good, we got the data in a dictionary.
-        return CACHE_DICTION
-    except:
-        CACHE_DICTION = {}
-        return CACHE_DICTION
+    return 'https://api.yelp.com/v3/businesses/search?location=' + location + '&limit=50'
 
-def write_cache(CACHE_FNAME, CACHE_DICT):
+def request_data(url):
 
-    jsonDict = json.dumps(CACHE_DICT)
-    f = open(CACHE_FNAME, "w")
-    f.write(jsonDict)
-    f.close()
+    headers = {'Authorization': 'Bearer %s' % API_KEY,}
+    r = requests.get(url, headers=headers)
+    return r.text
+
+def setUpDatabase(db_name):
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+
+    cur.execute("DROP TABLE IF EXISTS Restaurants")
+    cur.execute('CREATE TABLE IF NOT EXISTS Restaurants ("restaurant_id" TEXT PRIMARY KEY, "name" TEXT, "location" TEXT, "category" TEXT, "rating" REAL, "price" TEXT)')
+
+    return cur, conn
+
+def addEntriesToDatabase(cur, conn, data, location):
+    
+    pass
 
 def main():
 
-    headers = {
-        'Authorization': 'Bearer %s' % API_KEY,
-    }
+    url = get_url('New York City')
+    data = request_data(url)
+    data = json.loads(data)
+    cur, conn = setUpDatabase("restaurants.db")
 
-    url = "https://api.yelp.com/v3/businesses/search?location=NYC&limit=50"
-    r = requests.get(url, headers=headers)
-    data = r.text
-    print(data)
+    print(data['businesses'])
 
 
 if __name__ == "__main__":
